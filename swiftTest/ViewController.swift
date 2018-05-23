690718ef7430ffeeb2163e20cb865923611f0e5c
Vikas <vikas@vikass-mac-mini.local>
2018-05-23 15:30:41 +0530

    Printf

--- swiftTest/ViewController.swift
+++ swiftTest/ViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("Vikas Chauhan")
     }
 
     override func didReceiveMemoryWarning()
